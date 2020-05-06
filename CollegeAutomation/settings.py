d66c6ff81fa1284e17799c493e04ef789077814f
Rahul Soni <43738448+rsoni8672@users.noreply.github.com>
2020-05-06 20:05:09 +0530

    Update settings.py

--- CollegeAutomation/settings.py
+++ CollegeAutomation/settings.py
@@ -139,12 +139,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rahul.soni@spit.ac.in'
 EMAIL_HOST_PASSWORD = '8433607052'
 
-firebaseConfig = {
-    "apiKey": "AIzaSyBvMU6j3Y_EB9wKODelaVxIO6rQvIFoHp4",
-    "authDomain": "collegeautomation-53c7e.firebaseapp.com",
-    "databaseURL": "https://collegeautomation-53c7e.firebaseio.com",
-    "projectId": "collegeautomation-53c7e",
-    "storageBucket": "collegeautomation-53c7e.appspot.com",
-    "messagingSenderId": "279475633755",
-    "appId": "1:279475633755:web:33abdeae519a0526c1c97e"
-  }
